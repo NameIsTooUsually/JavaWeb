77d8843d2c38289719342f19e1f8fe69cd43ca31
chenhao <852355361@qq.com>
2022-05-01 22:58:50 +0800

    JDBC练习2

--- day01-jabc/src/com/chenhao/test/JDBCDemo5_Resultset.java
+++ day01-jabc/src/com/chenhao/test/JDBCDemo5_Resultset.java
@@ -1,11 +1,13 @@
 package com.chenhao.test;
 
+import com.chenhao.pojo.Stu;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class JDBCDemo5_Resultset {
 
@@ -62,5 +64,40 @@ public class JDBCDemo5_Resultset {
         resultSet.close();
         statement.close();
         connection.close();
+    }
+    @Test
+    public void testResultseToArrayList() throws Exception {
+        //获取连接
+        String url = "jdbc:mysql:///db1?useSSL=false";
+        String useName = "root";
+        String passWord = "123abc.";
+        Connection connection = DriverManager.getConnection(url, useName, passWord);
+        // 定义sql
+        String sql = "select id,name,age,gender from stu;";
+        //获取statement对象
+        Statement statement = connection.createStatement();
+        //执行sql
+        ResultSet resultSet = statement.executeQuery(sql);
+        //创建一个集合
+        ArrayList<Stu> stus = new ArrayList<>();
+        //处理结果
+        while (resultSet.next()){
+            //表的一行数据
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            int age = resultSet.getInt("age");
+            String gender = resultSet.getString("gender");
+            //封装成一个对象
+            Stu stu = new Stu(id, name, age, gender);
+            //存入集合中
+            stus.add(stu);
+    }
+        //释放资源
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+        //打印集合
+        System.out.println(stus);
     }
 }
